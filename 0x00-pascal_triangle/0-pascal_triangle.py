340938eba16c69ffd536622c8635b193c9913716
rukkyme <communicatejoy@yhaoo.com>
2023-07-28 01:30:19 +0100

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
+    """fucntion to get the numbers of pascal triangle"""
     if n <= 0:
         return []
+    pascal_triangle = [0] * n
 
-    triangle = [[1]]
+    for i in range(n):
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+		
+		for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
 
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
+        pascal_triangle[i] = new_row
 
-        for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-
-        new_row.append(1)
-        triangle.append(new_row)
-
-    return triangle
-
-# function is being called
-result = pascal_triangle(5)
-print(result)
+    return pascal_triangle
 
