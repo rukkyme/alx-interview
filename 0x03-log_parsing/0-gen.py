0f34657feb30cc7d2d80024a4b9b833bc67d78a3
rukkyme <communicatejoy@yahoo.com>
2023-08-20 23:20:16 +0000

    log parsing

--- 0x03-log_parsing/0-gen.py
+++ 0x03-log_parsing/0-gen.py
@@ -1,30 +1,16 @@
 #!/usr/bin/python3
+import random
 import sys
-total_size = count = 0
-# 200, 301, 400, 401, 403, 404, 405 and 500
-codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+from time import sleep
+import datetime
 
-
-def print_stats():
-    print("File size: {}".format(total_size))
-    for key, val in codes.items():
-        if val:
-            print("{}: {}".format(key, val))
-
-
-try:
-    for line in sys.stdin:
-        values = line.split(" ")
-        if len(values) > 2:
-            code = values[-2]
-            size = int(values[-1])
-            if code in codes:
-                codes[code] += 1
-            total_size += size
-            count += 1
-            if count == 10:
-                count = 0
-                print_stats()
-finally:
-    print_stats()
+for i in range(15):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(
+            1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
